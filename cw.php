50b648c13c3ea1504aeee5de7be634bedeccbfce
mahdi20021381 <84576769+mahdi20021381@users.noreply.github.com>
2021-05-21 22:57:06 +0430

    Add files via upload

---
+++ cw.php
@@ -0,0 +1,27 @@
+<html>
+<head>
+<title> CW Homework </title>
+</head>
+<body>
+<?php
+echo "<p><b> My Biography:</b></p>";
+echo "<u><i>My name is Mahdi  Alaei.</i></u>";
+echo "I am 18 years old.";
+echo "I Live In Shahmirzad.";
+echo "I Am A Student Of Computer Engineering At Semnan University.";
+echo "<p> <b>My  Resume:</b></p>";
+echo "<p><u>1-Academic Resume:</u></p>";
+echo "I Studied Pakzad High School.My Grade Point Average Was 18.53";
+echo "<p><u>2-Skills Resume:</u></p>";
+echo "I became the first person in the province in the field of Sahifa Sajjadieh";
+echo "<p><b> My Email Adress:</b></p>";
+echo "<p><u>mahdi1381alaee2002@gmail.com</u></p>";
+echo "<p><b> My instagram page:</b></p>";
+echo "<p><u>mahdi.alaei2002</u></p>";
+
+?>
+<p><a href="http://semnan.ac.ir"> My University </a></p>
+<p><a href="http://soft98.ir"> My beloved site</a></p>
+
+</body>
+</html>
